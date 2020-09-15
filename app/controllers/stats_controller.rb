7d5370ee3f264aba17c0888e392090b418066d99
Ramans <arudra.raman@gmail.com>
2020-09-15 16:25:43 +0530

    bitly gem added and shorten url feature implemented.

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,2 +1,50 @@
 class StatsController < ApplicationController
+
+    def index
+    end
+
+    def new
+    end
+
+    def short_url
+        response = Hash.new
+        client = Bitly::API::Client.new(token: "cb454e48cd6ecf351101693af7a86df1f9575064")
+
+        if params[:long_url].present?
+            begin
+                @stat = Stat.find_by_url(params[:long_url])
+
+                if @stat.present?
+                    response['short_url'] = @stat.shorten_url
+                else
+                    bitly = client.shorten(long_url: params[:long_url])
+                    response['short_url'] = bitly.link
+
+                    @stat = Stat.new
+                    @stat.url = params[:long_url]
+                    @stat.shorten_url = bitly.link
+                    @stat.save
+                end
+
+                response['status'] = "ACK"
+                response['message'] = "Success"
+            rescue Exception => e
+                response['status'] = "NACK"
+                response['message'] = e.message
+            end
+        else
+            response['status'] = "NACK"
+            response['message'] = "Please enter a url"
+        end
+
+        respond_to do |format|
+            format.html
+            format.json { render json: response.to_json, :callback => params[:callback] }
+        end
+    end
+
+    def routing_404
+        render :file => "#{Rails.root}/public/404.html", :status => 404, :layout => true
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+    resources :stats, only: [:index, :new] do
+        collection do
+            get "short_url"
+        end
+    end
+
+    root :to => "stats#new"
+
+    match '*a', :to => 'stats#routing_404', via: :all
 end
